8d7fdab87902e4b2272336a0214de07bdd3798f1
Kristen Cabrera <40896596+kris10cabrera@users.noreply.github.com>
2022-10-05 19:49:47 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,126 @@
 import Head from "next/head";
+import { useState } from "react";
 import useSWR from "swr";
+import Button from "../Button";
 import fetcher from "../lib/fetcher";
 
 export default function Home() {
+  const [user, fetchUser] = useState(null);
+
   const { data } = useSWR("/api/recentlyPlayed", fetcher);
   if (!data) {
     return null;
   }
-  const { artists, genres } = data;
+  const artists = user ? user : data.artists;
 
   return (
     <div>
       <Head>
-        <title>favorite artists & genres</title>
+        <title>favorite artists</title>
         <meta
           name="description"
-          content="favorite artists & genres on Spotify from the last 6 months. created by kris10cabrera"
+          content="favorite artists on Spotify from the last 6 months. created by kris10cabrera"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>favorite artists & genres</h1>
+      <main
+        style={{
+          backgroundColor: "#efefef",
+          padding: 20,
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: 4,
+            alignItems: "center",
+            justifyContent: "space-evenly",
+          }}
+        >
+          {artists.map((artist) => {
+            return (
+              <div
+                key={artist.artist}
+                className="artist-wrapper"
+                style={{
+                  width: 300,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  textAlign: "center",
+                }}
+              >
+                <a
+                  href={artist.url}
+                  className="artist-name"
+                  style={{
+                    fontSize: 28,
+                    lineHeight: 1.2,
+                    fontFamily: "FT88 Gothique",
+                  }}
+                >
+                  {artist.artist}
+                </a>
 
-        <div style={{ display: "flex", flexWrap: "wrap", gap: 10 }}>
-          {artists.map((artist) => (
-            <div key={artist.artist}>
-              <h2 style={{ fontStyle: "italic", fontSize: 20 }}>
-                {artist.artist}
-              </h2>
-              <img src={artist.image} style={{ width: 150 }} />
-            </div>
-          ))}
-          <div style={{ display: "flex", flexWrap: "wrap", gap: 10 }}>
-            {genres.map((genre) => (
-              <div key={genre}>{genre}</div>
-            ))}
+                <div
+                  className="artist-image"
+                  style={{
+                    backgroundImage: `url(${artist.image})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    width: 260,
+                    height: 260,
+                    maskImage: `url(/heart.png)`,
+                    maskRepeat: "no-repeat",
+                    maskPosition: "center",
+                    maskSize: "contain",
+                    WebkitMaskImage: `url(/heart.png)`,
+                    WebkitMaskRepeat: "no-repeat",
+                    WebkitMaskPosition: "center",
+                    WebkitMaskSize: "contain",
+                  }}
+                />
+              </div>
+            );
+          })}
+          <div
+            className="text-wrapper"
+            style={{
+              fontFamily: "Times",
+              position: "fixed",
+              left: "50%",
+              top: "50%",
+              transform: "translate(-50%, -50%)",
+              fontSize: 60,
+              width: "90%",
+              color: "red",
+              letterSpacing: -3,
+              maxWidth: 620,
+              border: "1px solid red",
+              background: "#ffffffcc",
+              padding: 20,
+              borderRadius: 11,
+              backdropFilter: "blur(3px)",
+            }}
+          >
+            <h1 style={{ display: "inline" }}>
+              this is what i’ve been listening to on spotify for the past 6
+              months{` `}
+            </h1>
+            <span
+              style={{
+                fontFamily: "FA_KJNZEIIUUL",
+                overflowWrap: "break-word",
+              }}
+            >
+              onmed
+            </span>
+            <Button fetchUser={fetchUser} />
+            <button>X</button>
           </div>
         </div>
       </main>
