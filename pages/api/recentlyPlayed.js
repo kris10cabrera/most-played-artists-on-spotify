3cffe578aa58ae5b58e647c3caef003628e3f292
Kristen Cabrera <40896596+kris10cabrera@users.noreply.github.com>
2022-10-05 19:49:12 -0400

    remove `genres`

--- pages/api/recentlyPlayed.js
+++ pages/api/recentlyPlayed.js
@@ -5,11 +5,11 @@ export default async (_, res) => {
   const response = await getRecentlyPlayed();
   const { items } = await response.json();
 
-  const genres = Array.from(new Set(items.map((item) => item.genres).flat()));
   const artists = items.map((item) => ({
     artist: item.name,
     image: item.images[1].url,
+    url: item.external_urls["spotify"],
   }));
 
-  return res.status(200).json({ genres, artists });
+  return res.status(200).json({ artists });
 };
