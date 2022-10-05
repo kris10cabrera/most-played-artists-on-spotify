23c51d79fa584c79c8079e10065f4f5b07a48a83
Kristen Cabrera <40896596+kris10cabrera@users.noreply.github.com>
2022-10-05 19:49:22 -0400

    remove genres from meta text

--- pages/_document.js
+++ pages/_document.js
@@ -12,13 +12,13 @@ class MyDocument extends Document {
         <Head>
           <meta
             name="description"
-            content="most played artists & genres on Spotify from the last 6 months. created by kris10cabrera"
+            content="most played artists on Spotify from the last 6 months. created by kris10cabrera"
           />
           <link rel="icon" href="/favicon.ico" />
-          <meta name="title" content="most played artists & genres" />
+          <meta name="title" content="most played artists" />
           <meta
             name="description"
-            content="most played artists & genres on Spotify from the last 6 months. created by kris10cabrera"
+            content="most played artists on Spotify from the last 6 months. created by kris10cabrera"
           />
           <meta property="og:type" content="website" />
         </Head>
