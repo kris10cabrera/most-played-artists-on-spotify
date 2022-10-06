80088b290c0b409004ef4d381df0a687691d305c
Kristen Cabrera <40896596+kris10cabrera@users.noreply.github.com>
2022-10-06 18:31:38 -0400

    favicons

--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,24 @@ class MyDocument extends Document {
             content="most played artists on Spotify from the last 6 months. created by kris10cabrera"
           />
           <meta property="og:type" content="website" />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
