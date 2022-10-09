fce0d23db92943af2d520fc101e31797e83f7e25
Kristen Cabrera <40896596+kris10cabrera@users.noreply.github.com>
2022-10-09 19:16:56 -0400

    💔

--- components/Heart.js
+++ components/Heart.js
@@ -1,96 +1,87 @@
 import Box from "./base/Box";
+import Image from "./base/Image";
 
 export default function Heart(props) {
-  const { artist } = props;
+  const { artist, index } = props;
+  const divisible = index % 3 === 0;
   return (
     <Box
+      as="a"
+      href={artist.url}
       key={artist.artist}
       css={{
+        position: "relative",
         width: 300,
         display: "flex",
         flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        textAlign: "center",
-        zIndex: 1,
-        transition: "transform 100ms",
+        alignItems: divisible ? "center" : "flex-end",
+        filter: "drop-shadow(red 0px 0px 10px)",
+        "&::after": {
+          content: "",
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          left: 0,
+          right: 0,
+          borderRadius: "71px",
+          transition: "opacity 300ms",
+          opacity: divisible ? 1 : 0,
+          zIndex: -2,
+          boxShadow:
+            "inset 0px 33px 25px 0 #000, inset 0 66px 15px 0px #ccc, inset 0 99px 5px 0px #fff",
+          filter: "drop-shadow(red 0px 0px 10px)",
+        },
+        "@hover": {
+          "&:hover": {
+            "&::after": {
+              opacity: divisible ? 0 : 1,
+            },
+          },
+        },
+        "@bp0-max": {
+          width: 300,
+        },
       }}
     >
       <Box
-        as="a"
-        href={artist.url}
         css={{
           fontSize: 21,
           lineHeight: 1.2,
           fontFamily: "FT88 Gothique",
-          zIndex: 3,
           color: "#fff",
           background: "#000",
           maxWidth: "100%",
           whiteSpace: "nowrap",
           textOverflow: "ellipsis",
           overflow: "hidden",
-          paddingTop: 2,
+          paddingTop: 4,
           paddingLeft: 4,
           paddingRight: 4,
-          borderRadius: "4px",
+          borderRadius: 4,
+          "@bp0-max": {
+            fontSize: 18,
+          },
         }}
       >
         {artist.artist}
       </Box>
-      <Box
+      <Image
+        alt={artist.name}
+        src={artist.image}
         css={{
-          width: 260,
-          height: 240,
-          position: "relative",
-          filter: "drop-shadow(red 0px 0px 10px)",
-          background: "#fff",
-          borderRadius: "71px",
-
-          "&::after": {
-            content: "",
-            position: "absolute",
-            width: 260,
-            height: 240,
-            left: 0,
-            right: 0,
-            borderRadius: "71px",
-            bottom: 0,
-            transition: "opacity 300ms",
-            opacity: 1,
-            zIndex: "-2",
-            boxShadow:
-              "inset 0px 33px 25px 0 #000, inset 0 66px 15px 0px #ccc, inset 0 99px 5px 0px #fff",
-            filter: "drop-shadow(red 0px 0px 10px)",
-          },
-          "@hover": {
-            "&:hover": {
-              "&::after": {
-                opacity: 0,
-              },
-            },
-          },
+          mixBlendMode: "color-burn",
+          clipPath:
+            index % 3 === 0
+              ? "path('M213.1,6.7c-32.4-14.4-73.7,0-88.1,30.6C110.6,4.9,67.5-9.5,36.9,6.7C2.8,22.9-13.4,62.4,13.5,110.9 C33.3,145.1,67.5,170.3,125,217c59.3-46.7,93.5-71.9,111.5-106.1C263.4,64.2,247.2,22.9,213.1,6.7z')"
+              : "url(#svgClip)",
         }}
-      >
-        <Box
-          className="artist-image"
-          css={{
-            background: "blue",
-            backgroundImage: `url(${artist.image})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            width: 240,
-            height: 236,
+      />
 
-            margin: "auto",
-            "@bp0-max": {
-              width: 80,
-              height: 80,
-            },
-          }}
-        ></Box>
-      </Box>
+      <svg width="0" height="0">
+        <clipPath id="svgClip" clipPathUnits="objectBoundingBox">
+          <path d="M0.75815095,0.0579477769 C1.597983708,0.187288937 0.902165272,0.77587654 0.799370955,0.785996249 C0.627963035,10.966765889 0.26163708,0.91434951 0.111342491,0.755791573 C-0.932137967,0.603287436 -0.035795248,0.382887577 0.0965066612,0.173955315 C0.200239457,0.10101396315 0.648923894,-0.0580965318 0.75815095,0.0579477 "></path>{" "}
+        </clipPath>
+      </svg>
     </Box>
   );
 }
