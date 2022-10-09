4358aaf38498c2ae7a3968feac20c33ce644e81d
Kristen Cabrera <40896596+kris10cabrera@users.noreply.github.com>
2022-10-09 19:52:31 -0400

    Create Heading.js

---
+++ components/Heading.js
@@ -0,0 +1,34 @@
+import Box from "./base/Box";
+
+export default function Heading() {
+  return (
+    <Box
+      as="h1"
+      css={{
+        fontFamily: "Times",
+        letterSpacing: -3,
+        zIndex: 5,
+        top: 15,
+        fontSize: 30,
+        letterSpacing: -1,
+        position: "sticky",
+        color: "#fff",
+        mixBlendMode: "color-burn",
+        border: "1px solid red",
+        background: "#000",
+        borderRadius: 11,
+        backdropFilter: "blur(3px)",
+        marginBottom: 40,
+        padding: 5,
+        "@bp0": {
+          padding: 20,
+          fontSize: 76,
+          position: "sticky",
+          letterSpacing: -3,
+        },
+      }}
+    >
+      these are my spotify crushes of the last 6 months.
+    </Box>
+  );
+}
