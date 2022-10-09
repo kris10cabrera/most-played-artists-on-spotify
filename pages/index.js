b9f403479db104c3357a2699f95beac13f65a69d
Kristen Cabrera <40896596+kris10cabrera@users.noreply.github.com>
2022-10-09 19:53:15 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Button from "../components/Button";
 import Heart from "../components/Heart";
 import Scrollbar from "../components/Doodles";
 import fetcher from "../lib/fetcher";
+import Heading from "../components/Heading";
 
 export default function Home() {
   const [user, fetchUser] = useState(null);
@@ -16,15 +17,14 @@ export default function Home() {
     return null;
   }
   const artists = user ? user : data.artists;
-  console.log(artists);
 
   return (
     <div>
       <Head>
-        <title>favorite artists</title>
+        <title>spotify crushes</title>
         <meta
           name="description"
-          content="favorite artists on Spotify from the last 6 months. created by kris10cabrera"
+          content="favorite artists on spotify from the last 6 months. created by kris10cabrera"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -38,48 +38,23 @@ export default function Home() {
       >
         <Scrollbar />
         <Box as="section" css={{ maxWidth: 1400, margin: "auto" }}>
-          <h1
-            style={{
-              fontFamily: "Times",
-              letterSpacing: -3,
-              zIndex: 5,
-              position: "sticky",
-              left: 5,
-              top: 15,
-              right: 5,
-              fontSize: 73,
-              color: "#7fc0ff",
-              mixBlendMode: "color-dodge",
-              border: "1px solid red",
-              background: "#ffffffcc",
-              padding: 20,
-              borderRadius: 11,
-              backdropFilter: "blur(3px)",
-              margin: "auto",
-              marginBottom: 40,
-            }}
-          >
-            this is what i’ve been listening to on spotify for the past 6 months{" "}
-            {` `}
-          </h1>
-
+          <Heading />
           <Button fetchUser={fetchUser} />
-          <button>X</button>
-
           <div
             style={{
               display: "flex",
               flexWrap: "wrap",
-              gap: 4,
+              gap: 10,
               alignItems: "center",
               justifyContent: "space-evenly",
             }}
           >
-            {artists.map((artist) => {
-              return <Heart artist={artist} key={artist.image} />;
+            {artists.map((artist, index) => {
+              return <Heart artist={artist} key={artist.image} index={index} />;
             })}
           </div>
         </Box>
+        encuentra fuerza en el sonido
       </Box>
     </div>
   );
