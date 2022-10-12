d46f2dadc0d79831dd1456c2261b1301193a19d1
Kristen Cabrera <40896596+kris10cabrera@users.noreply.github.com>
2022-10-12 19:17:25 -0400

    font size updates

--- components/SecondaryHeading.js
+++ components/SecondaryHeading.js
@@ -6,13 +6,13 @@ export default function SecondaryHeading() {
       as="h2"
       css={{
         fontFamily: "FT88 Gothique",
-        fontSize: 27,
         color: "#000",
         textShadow: "0 0 7px white",
+        fontSize: 27,
         paddingTop: 10,
         marginBottom: 15,
         "@bp0-max": {
-          fontSize: 16,
+          fontSize: 23,
         },
       }}
     >
