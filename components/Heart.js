f62c28cd7486dc35d98e3114bd952bc55eb9ad8a
Kristen Cabrera <40896596+kris10cabrera@users.noreply.github.com>
2022-10-12 14:22:59 -0400

    Update Heart.js

--- components/Heart.js
+++ components/Heart.js
@@ -27,6 +27,8 @@ export default function Heart(props) {
           height: "100%",
           left: 0,
           right: 0,
+          top: 0,
+          bottom: 0,
           borderRadius: "71px",
           transition: "opacity 300ms",
           opacity: 1,
