bb137696353df09d60fad4a4646131005d8200b9
Kristen Cabrera <40896596+kris10cabrera@users.noreply.github.com>
2022-10-12 20:09:29 -0400

    doodles gone mad

--- components/Doodles.js
+++ components/Doodles.js
@@ -30,6 +30,28 @@ export default function Scrollbar() {
       onmedonmedonmedonmedonmedonmedonmed onmedonmedonmedonmedonmedonmedonmed
       onmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmedonmed
       onmedonmedonmedonmedonmedonmedonmed
+      {arr.map((elem, index) => {
+        const divisible = index % 2 === 0;
+        return (
+          <Box
+            as="span"
+            key={index}
+            css={{
+              borderRadius: divisible ? 300 : 20,
+              border: divisible ? "1px solid #19ff00" : "1px solid #fff",
+              boxShadow: divisible
+                ? "#b2b2b2 1px 1px 18px 0px inset"
+                : "rgb(178 178 178) 1px 1px 8px 1px inset",
+              color: "#b2b2b2",
+              transform: divisible
+                ? "rotate(90deg) rotateY(-45deg) skewX(-10deg)"
+                : "rotate(33deg) scaleY(1.5)",
+            }}
+          >
+            {elem}
+          </Box>
+        );
+      })}
     </Box>
   );
 }
