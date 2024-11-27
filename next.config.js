2c1a910e0d57abddbf1f6d3b83a63600d31133c1
Kristen Cabrera <cabrera.kristen@gmail.com>
2024-11-27 17:38:23 +0100

    bump

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
 /** @type {import('next').NextConfig} */
-
 module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.scdn.co",
-        port: "",
-        pathname: "/image/**",
-      },
-    ],
-  },
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		unoptimized: true,
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "i.scdn.co",
+				port: null,
+				pathname: "/image/**",
+			},
+		],
+	},
 };
